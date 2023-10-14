8bb37dc06db8b6f77ab41cbed414b941b9e600df
Gagr300 <gromovag26784@yandex.ru>
2023-10-15 00:32:16 +0400

    added task1

--- project-template/src/main/java/edu/hw1/Task1.java
+++ project-template/src/main/java/edu/hw1/Task1.java
@@ -1,10 +1,23 @@
 package edu.hw1;
 
+import java.util.logging.Logger;
+import java.util.Arrays;
+
 public class Task1 {
+
+    //Task0
+    private static final Logger LOGGER = Logger.getLogger(Task1.class.getName());
+
+    public static void main (String[] args) {
+        LOGGER.info("Привет, мир!");
+    }
+
+
+    //Task1
     public static Integer minutesToSeconds(String time){
         String[] nums = time.split(":");
 
-        if (nums[0].length() < 2 | nums[1].length() < 2)
+        if ( nums[1].length() < 2 | nums.length != 2)
             return -1;
 
 
@@ -17,4 +30,127 @@ public class Task1 {
         return minutes * 60 + seconds;
     }
 
+    //Task2
+    public static int countDigits(int num) {
+        if (num < 0) num = Math.abs(num);
+        if (num < 10) return 1;
+        return 1 + countDigits(num / 10);
+    }
+
+    //Task3
+    public static Boolean isNestable(int[] ms1, int[] ms2){
+        if (ms1.length == 0 | ms2.length == 0) return false;
+        return Arrays.stream(ms1).max().orElseThrow() < Arrays.stream(ms2).max().orElseThrow() &
+            Arrays.stream(ms1).min().orElseThrow() > Arrays.stream(ms2).min().orElseThrow();
+    }
+
+    //Task4
+    public static StringBuilder fixString(String str) {
+        StringBuilder strAns = new StringBuilder();
+        for (int i = 0; i < str.length() - 1; i += 2) {
+            strAns.append(str.charAt(i + 1));
+            strAns.append(str.charAt(i));
+        }
+        if (str.length() % 2 != 0)
+            strAns.append(str.charAt(str.length() - 1));
+
+        return strAns;
+    }
+
+    //Task5
+    public static boolean isPalindromeDescendant(int num) {
+        if (num < 10) return false;
+        String str = String.valueOf(num);
+        return isPalindromeDescendant(str);
+
+    }
+
+    public static boolean isPalindromeDescendant(String str) {
+        if (str.length() == 1) return false;
+        boolean flag = true;
+        for (int i = 0; i < str.length(); i++)
+            flag &= (str.charAt(str.length() - 1 - i) == str.charAt(i));
+        if (flag) return true;
+        else {
+            StringBuilder newstr = new StringBuilder();
+            for (int i = 0; i < str.length(); i += 2)
+                newstr.append(Integer.parseInt("" + str.charAt(i))
+                    + Integer.parseInt("" + str.charAt(i + 1)));
+            return isPalindromeDescendant(newstr.toString());
+        }
+    }
+
+    //Task6
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+
+    public static int countK(int num) {
+        if (num <= 1000) return -1;
+        int ans =0;
+        while (num != 6174){
+
+            char[] str = Integer.toString(num).toCharArray();
+            Arrays.sort(str);
+            int des = Integer.parseInt(String.valueOf(str));
+            int inc = Integer.parseInt(reverseString(new String(str)));
+            num = inc - des;
+            ans++;
+        }
+        return ans;
+    }
+
+    //Task7
+    public static String toBinaryString(int n)
+    {
+        return String.format("%16s", Integer.toBinaryString(n)).replaceAll(" ", "0");
+    }
+
+    public static int findNextPowerOf2(int n)
+    {
+        n--;
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+        return ++n;
+    }
+    public static int rotateLeft(int n, int shift){
+        return ((n << shift) | (n >> ((int)  ((Math.ceil((Math.log(n) + Math.log(2) - 1)/Math.log(2))) + 1 - shift))))
+            & ( findNextPowerOf2(n) - 1);
+    }
+    public static int rotateRight(int n, int shift){
+        return ((n >> shift) | (n << ((int) (Math.ceil((Math.log(n) + Math.log(2) - 1) / Math.log(2))) + 1 - shift)))
+            & ( findNextPowerOf2(n) - 1);
+    }
+
+    //Task8
+    public static boolean check(int x, int y) {
+        return  x >=0 & x<8 & y >=0 & y <8;
+    }
+
+    private static final int[] xx = new int[]{-1,-2,-1,-2,1,2,1,2};
+
+    private static final int[] yy = new int[]{-2,-1,2,1,2,1,-2,-1};
+
+    public static boolean knightBoardCapture(int[][] field) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (field[i][j] == 1)
+                    for (int u = 0; u < 8; u++) {
+                        int x = i + xx[u];
+                        int y = j + yy[u];
+                        if (check(x, y)) {
+                            if (field[x][y] == 1) {
+                                //System.out.println(i + " " + j + " " + x + " " + y);
+                                return false;
+                            }
+                        }
+                    }
+            }
+        }
+        return true;
+    }
 }
--- project-template/src/test/java/edu/hw1/SampleTest.java
+++ project-template/src/test/java/edu/hw1/SampleTest.java
@@ -2,17 +2,351 @@ package edu.hw1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SampleTest {
 
+    //Task1
+
     @Test
-    @DisplayName("Ф")
-    void minutesToSeconds() {
+    @DisplayName("true minutesToSeconds 0")
+    void minutesToSecondsT0() {
         String in = "01:00";
-        int out = 60;
+        int expectedResult = 60;
         int actualResult = Task1.minutesToSeconds(in);
-        // then
-        assertThat(actualResult).isEqualTo(out);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("true minutesToSeconds 1")
+    void minutesToSecondsT1() {
+        String in = "13:56";
+        int expectedResult = 836;
+        int actualResult = Task1.minutesToSeconds(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("true minutesToSeconds 2")
+    void minutesToSecondsT2() {
+        String in = "100:00";
+        int expectedResult = 6000;
+        int actualResult = Task1.minutesToSeconds(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("false minutesToSeconds 0")
+    void minutesToSecondsT3() {
+        String in = "01:00:00";
+        int expectedResult = -1;
+        int actualResult = Task1.minutesToSeconds(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("false minutesToSeconds 1")
+    void minutesToSecondsT4() {
+        String in = "10:60";
+        int expectedResult = -1;
+        int actualResult = Task1.minutesToSeconds(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("false minutesToSeconds 2")
+    void minutesToSecondsT5() {
+        String in = "-10:20";
+        int expectedResult = -1;
+        int actualResult = Task1.minutesToSeconds(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("false minutesToSeconds 3")
+    void minutesToSecondsT6() {
+        String in = "10:-06";
+        int expectedResult = -1;
+        int actualResult = Task1.minutesToSeconds(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("false minutesToSeconds 4")
+    void minutesToSecondsT7() {
+        String in = "100:1";
+        int expectedResult = -1;
+        int actualResult = Task1.minutesToSeconds(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    //Task2
+    @Test
+    @DisplayName("countDigits")
+    void countDigitsT0() {
+        int in = 4666;
+        int expectedResult = 4;
+        int actualResult = Task1.countDigits(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("countDigits")
+    void countDigitsT1() {
+        int in = 544;
+        int expectedResult = 3;
+        int actualResult = Task1.countDigits(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("countDigits")
+    void countDigitsT2() {
+        int in = 0;
+        int expectedResult = 1;
+        int actualResult = Task1.countDigits(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("countDigits")
+    void countDigitsT3() {
+        int in = -100;
+        int expectedResult = 3;
+        int actualResult = Task1.countDigits(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    //Task3
+
+    @Test
+    @DisplayName("true isNestable 0")
+    void isNestableT0() {
+        int[] in0 = new int[] {1, 2, 3, 4};
+        int[] in1 = new int[] {0, 6};
+        boolean expectedResult = true;
+        boolean actualResult = Task1.isNestable(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("true isNestable 1")
+    void isNestableT1() {
+        int[] in0 = new int[] {3, 1};
+        int[] in1 = new int[] {4, 0};
+        boolean expectedResult = true;
+        boolean actualResult = Task1.isNestable(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("false isNestable 0")
+    void isNestableT2() {
+        int[] in0 = new int[] {9, 9, 8};
+        int[] in1 = new int[] {8, 9};
+        boolean expectedResult = false;
+        boolean actualResult = Task1.isNestable(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("false isNestable 1")
+    void isNestableT3() {
+        int[] in0 = new int[] {1, 2, 3, 4};
+        int[] in1 = new int[] {2, 3};
+        boolean expectedResult = false;
+        boolean actualResult = Task1.isNestable(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("false isNestable 2")
+    void isNestableT4() {
+        int[] in0 = new int[] {1, 2, 3, 4};
+        int[] in1 = new int[] {};
+        boolean expectedResult = false;
+        boolean actualResult = Task1.isNestable(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    //Task4
+    @Test
+    @DisplayName("even fixString")
+    void fixStringT0() {
+        String in = "123456";
+        String expectedResult = "214365";
+        String actualResult = String.valueOf(Task1.fixString(in));
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("even fixString")
+    void fixStringT1() {
+        String in = "hTsii  s aimex dpus rtni.g";
+        String expectedResult = "This is a mixed up string.";
+        String actualResult = String.valueOf(Task1.fixString(in));
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("odd fixString")
+    void fixStringT2() {
+        String in = "badce";
+        String expectedResult = "abcde";
+        String actualResult = String.valueOf(Task1.fixString(in));
+        assertEquals(expectedResult, actualResult);
+    }
+
+    //Task5
+    @Test
+    @DisplayName("isPalindromeDescendantT0")
+    void isPalindromeDescendantT0() {
+        int in = 11211230;
+        boolean expectedResult = true;
+        boolean actualResult = Task1.isPalindromeDescendant(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("isPalindromeDescendantT1")
+    void isPalindromeDescendantT1() {
+        int in = 13001120;
+        boolean expectedResult = true;
+        boolean actualResult = Task1.isPalindromeDescendant(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("isPalindromeDescendantT2")
+    void isPalindromeDescendantT2() {
+        int in = 23336014;
+        boolean expectedResult = true;
+        boolean actualResult = Task1.isPalindromeDescendant(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("isPalindromeDescendantT3")
+    void isPalindromeDescendantT3() {
+        int in = 11;
+        boolean expectedResult = true;
+        boolean actualResult = Task1.isPalindromeDescendant(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("false isPalindromeDescendantF0")
+    void isPalindromeDescendanF0() {
+        int in = 12;
+        boolean expectedResult = false;
+        boolean actualResult = Task1.isPalindromeDescendant(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("false isPalindromeDescendantF0")
+    void isPalindromeDescendanF1() {
+        int in = -11;
+        boolean expectedResult = false;
+        boolean actualResult = Task1.isPalindromeDescendant(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    //Task6
+    @Test
+    @DisplayName("countKT0")
+    void countKT0() {
+        int in = 6621;
+        int expectedResult = 5;
+        int actualResult = Task1.countK(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("countKT1")
+    void countKT1() {
+        int in = 6554;
+        int expectedResult = 4;
+        int actualResult = Task1.countK(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("countKT2")
+    void countKT2() {
+        int in = 1234;
+        int expectedResult = 3;
+        int actualResult = Task1.countK(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    //Task7
+    @Test
+    @DisplayName("rotateRightT0")
+    void rotateRightT0() {
+        int in0 = 8;
+        int in1 = 1;
+        int expectedResult = 4;
+        int actualResult = Task1.rotateRight(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("rotateLeftT0")
+    void rotateLeftT0() {
+        int in0 = 16;
+        int in1 = 1;
+        int expectedResult = 1;
+        int actualResult = Task1.rotateLeft(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("rotateLeftT1")
+    void rotateLeftT1() {
+        int in0 = 17;
+        int in1 = 2;
+        int expectedResult = 6;
+        int actualResult = Task1.rotateLeft(in0, in1);
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    //Task8
+
+    @Test
+    @DisplayName("Right knightBoardCapture")
+    void knightBoardCaptureT() {
+        int[][] in = new int[][] {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+        boolean expectedResult = true;
+        boolean actualResult = Task1.knightBoardCapture(in);
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    @DisplayName("Wrong knightBoardCapture 0")
+    void knightBoardCaptureF0() {
+        int[][] in = new int[][] {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        boolean expectedResult = false;
+        boolean actualResult = Task1.knightBoardCapture(in);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Wrong knightBoardCapture 1")
+    void knightBoardCaptureF1() {
+        int[][] in = new int[][] {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        boolean expectedResult = false;
+        boolean actualResult = Task1.knightBoardCapture(in);
+        assertEquals(expectedResult, actualResult);
     }
 }
